cff0a00e560a3d99f275615faad43d39e142b610
MONLAU\martacalgua <martcalgua@campus.monlau.com>
2022-12-21 15:44:32 +0100

    New Folder has been added

---
+++ P1/Services/RepositoryOfProjects - Copia.cs
@@ -0,0 +1,51 @@
+﻿using P1_ASP.Models;
+
+namespace P1_ASP.Services
+{
+    public class RepositoryOfFormaciones
+    {
+        public List<ClassProjects> GetProjects()
+        {
+            return new List<ClassProjects>() 
+            {
+                new ClassProjects {
+                    Title = "Memory",
+                    Description = "Juego realizado con C#",
+                    Link = "https://amazon.com",
+                    ImageURL = "/images/memory.png"
+                },
+                new ClassProjects {
+                    Title = "Calculadora",
+                    Description = "Calculadora hecha con Java",
+                    Link = "https://Monlau.com",
+                    ImageURL = "/images/calculadora.png"
+                },
+                new ClassProjects {
+                    Title = "Concurso",
+                    Description = "Concurso realizado con JavaScript",
+                    Link = "https://Monlau.com",
+                    ImageURL = "/images/conurso.png"
+                },
+                new ClassProjects {
+                    Title = "Portfolio",
+                    Description = "Web relizada con HTML y CSS",
+                    Link = "https://Monlau.com",
+                    ImageURL = "/images/logoASP.png"
+                },
+                new ClassProjects {
+                    Title = "Zonas horarias",
+                    Description = "Web para visualizar la hora y la fecha en direfentes zonas horarias de Europa relizado con JavaScript",
+                    Link = "https://Monlau.com",
+                    ImageURL = "/images/logoASP.png"
+                },
+                new ClassProjects {
+                    Title = "Leyes de Gestalt",
+                    Description = "Pagina relizada con CSS y HTML",
+                    Link = "https://Monlau.com",
+                    ImageURL = "/images/logoASP.png"
+                }
+            };
+        }
+
+    }
+}
